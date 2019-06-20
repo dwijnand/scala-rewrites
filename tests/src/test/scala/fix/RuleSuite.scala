e55b3da5b468a6d22961f818f250d884e495bfa8
Dale Wijnand <dale.wijnand@gmail.com>
2019-06-20 17:15:15 +0100

    Merge the test runners

--- tests/src/test/scala/fix/RuleSuite.scala
+++ tests/src/test/scala/fix/RuleSuite.scala
@@ -1,7 +1,36 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
 package fix
 
-import scalafix.testkit.SemanticRuleSuite
+import scala.meta._
+import scalafix.v0._
+import scalafix.testkit._
 
-class RuleSuite extends SemanticRuleSuite() {
-  runAllTests()
+class ScalaRewritesTests extends SemanticRuleSuite {
+
+  val only: Option[String] =
+    // Some("Playground") // << to run only one test:
+    None
+
+  def testOnly(file: String): Unit = {
+    testsToRun
+      .filter(_.path.testPath.toNIO.getFileName.toString.stripSuffix(".scala") == file)
+      .foreach(runOn)
+  }
+
+  only match {
+    case Some(file) => testOnly(file)
+    case None       => runAllTests()
+  }
+
 }
